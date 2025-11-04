4aa5a4a836fbded118fb47cd59f1a260238dc1a0
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-11-04 18:57:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-//your code here!
-
 const listEl = document.querySelector('#infi-list');
 
-let itemNumber = 10;
-const threshold = 2;
+const thersold = 2;
+let initialItem = 1;
 
-function addItems(count) {
-	for (let i = 1; i <= count; i++) {
+function addItems(count){
+	for(let i=1; i<=count; i++){
 		const item = document.createElement('li');
-		item.textContent = `Item ${itemNumber + i}`;
+		item.textContent = `Item ${initialItem + i}`;
 		listEl.appendChild(item);
 	}
-	itemNumber += count;
+	initialItem += count;
 }
 
 addItems(10);
 
 listEl.addEventListener('scroll', (e) => {
-	let scrollTop = e.target.scrollTop;
-	let scrollHeight = e.target.scrollHeight;
-	let clientHeight = e.target.clientHeight;
-	let remainingScroll = scrollHeight - ( scrollTop - clientHeight);
+	const scrollTop = e.target.scrollTop;
+	const scrollHeight = e.target.scrollHeight;
+	const clientHeight = e.target.clientHeight;
 
-	if (remainingScroll <= threshold) {
-		addItems(2); // Add 2 more items
-	}
-});
+	const remainingScroll = scrollHeight - (scrollTop + clientHeight);
+
+	if(remainingScroll <= thersold){
+		addItems(thersold);
+	}	
+})
+
